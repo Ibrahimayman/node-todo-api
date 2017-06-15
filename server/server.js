998d189ca6a0393539de0e1923f50e82950bb400
ibrahimayman <eng.ibrahim.cer@hotmail.com>
2017-06-15 21:59:39 +0200

    make it better

--- server/server.js
+++ server/server.js
@@ -7,15 +7,29 @@ mongoose.promise = global.promise;
 mongoose.connect("mongodb://localhost:27017/TodoApp"); // mongoose contains connection all time
 
 var Todo = mongoose.model("Todo", {
-    text: {type: String},
-    completed: {type: Boolean},
-    completedAt: {type: Number}
+    text: {type: String, required: true, minlength: 1, trim: true},
+    completed: {type: Boolean, default: false},
+    completedAt: {type: Number, default: null}
 });
 
 var newTodo = new Todo({
     text: "cook Dinner"
 });
 
+var user = mongoose.model("user", {
+    email: {type: String, required: true, trim: true, minlength: 1}
+});
+
+var newUser = new user({email: "eng.ibrahim@hotmail.om"});
+
+// save user to database.
+newUser.save().then((doc) => {
+    console.log(doc);
+}, (e) => {
+    console.log(e);
+});
+
+// save data to database.
 newTodo.save().then((doc) => {
     console.log("saved todo " + doc);
 }, (e) => {
