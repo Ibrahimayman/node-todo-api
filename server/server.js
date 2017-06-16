65ea4e16c175b704d9f5426eaaba83b75346ff20
ibrahimayman <eng.ibrahim.cer@hotmail.com>
2017-06-16 11:24:17 +0200

    'update'

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@
  * Created by Ibrahim Ayman on 15/06/2017.
  */
 
-var express = require('express');
-var bodyParser = require("body-parser");
-var {ObjectID} = require("mongodb");
+const express = require('express');
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+const {ObjectID} = require("mongodb");
 
 // local imports
 var {mongoose} = require("./db/mongoose");
@@ -15,6 +16,7 @@ var {user} = require("./models/user");
 var app = express();
 app.use(bodyParser.json());
 
+// insert new document.
 app.post("/todos", (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -53,7 +55,7 @@ app.get("/todos/:id", (req, res) => {
     });
 });
 
-
+// delete document.
 app.delete("/todos/:id", (req, res) => {
     // get the id
     var id = req.params.id;
@@ -72,6 +74,29 @@ app.delete("/todos/:id", (req, res) => {
     });
 });
 
+// update document.
+app.patch("/todos/:id", (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    if (!ObjectID.isValid) {
+        res.status(400).send();
+    }
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    }
+    else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findOneAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            res.status(400).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(process.env.port || 3000, () => {
     console.log("app running");
