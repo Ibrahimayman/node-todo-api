bef643a86059ad9cadf31e1e6223c927d6604845
ibrahimayman <eng.ibrahim.cer@hotmail.com>
2017-06-18 10:37:06 +0200

    'login'

--- server/server.js
+++ server/server.js
@@ -113,8 +113,22 @@ app.post("/users", (req, res) => {
 });
 
 // get user
-app.get("/users/me", authenticate,(req,res)=>{
-res.send(req.user);
+app.get("/users/me", authenticate, (req, res) => {
+    res.send(req.user);
+});
+
+
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
 });
 
 app.listen(process.env.port || 3000, () => {
